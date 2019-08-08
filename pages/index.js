c691020d8338b5652d6ce84742b0c521bb57bf23
reuvenaor <reuvenaor85@gmail.com>
2019-08-08 20:54:46 +0300

    + svg

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,17 @@ import FirstPerson from '../components/firstPerson';
 import { config } from 'react-spring';
 import { Parallax, ParallaxLayer } from '../node_modules/react-spring/renderprops-addons.js'
 import { Spring } from '../node_modules/react-spring/renderprops'
+import BackOne from '../static/back1';
+import BackTwo from '../static/back2';
 
 function Index() {
 
+  const [win, setWindow] = useState(null);
+
   let parallax;
   useEffect(() => {
+    setWindow(window);
+    //console.log('window.height',window.screen.availHeight, window.screen.availWidth);
     // WORK WITH API - date.js
     // async function getDate() {
     //   const res = await fetch('/api/date');
@@ -23,45 +29,63 @@ function Index() {
       <Head>
         <title>Reuven 3D</title>
       </Head>
-      <Parallax ref={ref => (parallax = ref)} pages={3}>
-        <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} >
-          <FirstPerson />
+      <Parallax ref={ref => (parallax = ref)} pages={2} style={{ width: '100vw', maxWidth: '100vw', minWidth: '100vw' }}>
+        {/* <ParallaxLayer offset={1} speed={7} style={{ backgroundColor: '#805EFF' }} >
+          <BackTwo />
         </ParallaxLayer>
-        <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} >
-          <FirstPerson />
+        <ParallaxLayer offset={2} speed={7} style={{ backgroundColor: '#87BCDE' }} >
+          <BackOne />
+        </ParallaxLayer> */}
+
+
+        {win ? <ParallaxLayer
+          offset={0}
+          speed={2}
+          //factor={3}
+          style={{ width: '100vw' }}  >
+          <BackOne width={win.screen.availWidth} height={win.screen.availHeight} />
+        </ParallaxLayer> : null}
+        {win ? <ParallaxLayer
+          offset={0}
+          speed={4}
+          //factor={1}
+          style={{ pointerEvents: 'none', width: '100vw' }}>
+          <BackTwo width={win.screen.availWidth} height={win.screen.availHeight} />
+        </ParallaxLayer>
+          : null}
+        {win ? <ParallaxLayer
+          //offset={1}
+          speed={2}
+          //factor={1}
+          style={{ pointerEvents: 'none', width: '100vw' }}>
+          <BackTwo width={win.screen.availWidth} height={win.screen.availHeight} />
+        </ParallaxLayer>
+          : null}
+
+        {/* <ParallaxLayer offset={1} speed={6} style={{ opacity: 0.1 }}>
+          <BackOne />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={0} speed={0} factor={3} style={{ backgroundColor: '#87BCDE' }}  >
-          <FirstPerson />
-        </ParallaxLayer>
-        <ParallaxLayer offset={1.3} speed={0.3} style={{ pointerEvents: 'none' }}>
-          <FirstPerson />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
-          <FirstPerson />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
-          <FirstPerson />
+         <ParallaxLayer offset={1.75} speed={4} style={{ opacity: 0.1 }}>
+          <BackTwo />
         </ParallaxLayer>
 
 
         <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
-          <FirstPerson />
+          <BackOne />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
-          <FirstPerson />
-        </ParallaxLayer>
+          <BackOne />
+        </ParallaxLayer> 
 
         <ParallaxLayer offset={2.6} speed={-0.4} style={{ opacity: 0.6 }}>
-          <FirstPerson />
-        </ParallaxLayer>
+          <BackTwo />
+        </ParallaxLayer> */}
 
-        <ParallaxLayer offset={2.5} speed={-0.4} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', pointerEvents: 'none' }}>
-          <FirstPerson />
-        </ParallaxLayer>
+        {/* <ParallaxLayer offset={2.4} speed={-0.5} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', pointerEvents: 'none' }}>
+          <BackOne />
+        </ParallaxLayer> */}
 
       </Parallax>
 
@@ -72,10 +96,10 @@ function Index() {
           left: 0;
           top: 0;
           width: '100vw';
-          height: '100vh';
+          //height: '100vh';
           margin: 0;
           padding: 0;
-          display: grid;
+          //display: grid;
           // font-family: 'SF Pro Text', 'SF Pro Icons', 'Helvetica Neue',
           //  'Helvetica', 'Arial', sans-serif;
           // hyphens: auto;
@@ -85,11 +109,8 @@ function Index() {
           // max-width: 90%;
           // min-height: 90%;
           // padding: 72px 0;
-          // text-align: center;
-        }
-        body {
-          width: '100%';
-          height: '100%';
+          text-align: center;
+          align-items: center;
         }
         p {
           font-size: 16px;
