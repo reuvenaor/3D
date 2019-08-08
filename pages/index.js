2efc08542984614e65677a44ec7f6aeb6af255da
reuvenaor <reuvenaor85@gmail.com>
2019-08-08 22:27:02 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -29,62 +29,52 @@ function Index() {
       <Head>
         <title>Reuven 3D</title>
       </Head>
-      <Parallax ref={ref => (parallax = ref)} pages={2} style={{ width: '100vw', maxWidth: '100vw', minWidth: '100vw' }}>
-        {/* <ParallaxLayer offset={1} speed={7} style={{ backgroundColor: '#805EFF' }} >
-          <BackTwo />
-        </ParallaxLayer>
-        <ParallaxLayer offset={2} speed={7} style={{ backgroundColor: '#87BCDE' }} >
-          <BackOne />
-        </ParallaxLayer> */}
+      <Parallax ref={ref => (parallax = ref)} pages={4} style={{ width: '100vw', maxWidth: '100vw', minWidth: '100vw' }}>
+        <ParallaxLayer offset={0} speed={-1} style={{ backgroundColor: '#11BCDE' }} />
+        <ParallaxLayer offset={0} speed={-6} style={{ backgroundColor: '#FFBCDE' }} />
+        <ParallaxLayer speed={6} style={{ backgroundColor: '#0000EE', opacity: 0.2 }} />
 
-
-        {win ? <ParallaxLayer
-          offset={0}
-          speed={2}
-          //factor={3}
-          style={{ width: '100vw' }}  >
-          <BackOne width={win.screen.availWidth} height={win.screen.availHeight} />
-        </ParallaxLayer> : null}
-        {win ? <ParallaxLayer
-          offset={0}
-          speed={4}
-          //factor={1}
-          style={{ pointerEvents: 'none', width: '100vw' }}>
-          <BackTwo width={win.screen.availWidth} height={win.screen.availHeight} />
+        <ParallaxLayer offset={1} speed={0} factor={3} style={{ width: '100vw', maxWidth: '100vw', minWidth: '100vw', backgroundColor: '#ffBCDE'  }}>
+          <BackTwo/>
         </ParallaxLayer>
-          : null}
-        {win ? <ParallaxLayer
-          //offset={1}
-          speed={2}
-          //factor={1}
-          style={{ pointerEvents: 'none', width: '100vw' }}>
-          <BackTwo width={win.screen.availWidth} height={win.screen.availHeight} />
+        
+        <ParallaxLayer offset={0} speed={3} factor={0} style={{ width: '100vw', maxWidth: '100vw', minWidth: '100vw' }}>
+          <BackOne/>
         </ParallaxLayer>
-          : null}
+        
 
-        {/* <ParallaxLayer offset={1} speed={6} style={{ opacity: 0.1 }}>
-          <BackOne />
+        {/* <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
+          <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} />
         </ParallaxLayer>
 
-         <ParallaxLayer offset={1.75} speed={4} style={{ opacity: 0.1 }}>
-          <BackTwo />
+        <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
+          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
+          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
         </ParallaxLayer>
 
+        <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
+          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
+          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
+        </ParallaxLayer>
 
         <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
-          <BackOne />
+          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
+          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '75%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
-          <BackOne />
-        </ParallaxLayer> 
+          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '60%' }} />
+          <img src={url('cloud')} style={{ display: 'block', width: '25%', marginLeft: '30%' }} />
+          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
+        </ParallaxLayer>
 
-        <ParallaxLayer offset={2.6} speed={-0.4} style={{ opacity: 0.6 }}>
-          <BackTwo />
-        </ParallaxLayer> */}
+        <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
+          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '5%' }} />
+          <img src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '75%' }} />
+        </ParallaxLayer>
 
-        {/* <ParallaxLayer offset={2.4} speed={-0.5} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', pointerEvents: 'none' }}>
-          <BackOne />
+        <ParallaxLayer offset={2.5} speed={-0.4} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', pointerEvents: 'none' }}>
+          <img src={url('earth')} style={{ width: '60%' }} />
         </ParallaxLayer> */}
 
       </Parallax>
