3d9ca53f52de7e2209f7d66cbc3bbba2099a6e0c
reuvenaor <reuvenaor85@gmail.com>
2019-08-11 00:25:01 +0300

    two more svg

---
+++ static/swimmer.js
@@ -0,0 +1,47 @@
+
+const swimmer = (props) => <svg version="1.1"
+	 xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px" viewBox="0 0 960 560" enable-background="new 0 0 960 560">
+<path id="XMLID_2_" fill="none" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+	M443.6,347.8c-3.7-6.8-3.8-14.9-3.3-22.6c0.7-10.6,2.5-21.5,8.1-30.6s15.6-16,26.3-15.3c6.1,0.4,11.8,3.1,17.3,6
+	c4.6,2.4,9.3,5,12.5,9.2c1.4,1.9,2.4,4,3.4,6.1c7,16.5,6.6,35.1,6.1,53"/>
+<path id="XMLID_3_" fill="none" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+	M443,341.9c2.6-3.8,7.5-5.4,12.1-5.4c4.6,0,9,1.4,13.5,2.5c4.5,1,9.2,1.7,13.6,0.3c2.8-0.9,5.2-2.6,7.9-3.6c5.4-2.1,11.5-1.7,17.3-1
+	c1,0.1,2,0.2,2.8-0.3c1.2-0.8,1.4-2.6,2.5-3.4c-0.6,0.9-1.2,1.8-1.7,2.8"/>
+<path id="XMLID_4_" fill="none" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+	M446.1,344.1c3.5,3.8,8,6.9,13,8c5,1.1,10.7,0.1,14.6-3.3c0.9-0.8,1.8-1.7,2.8-2.5c3-2.1,7.6-1.6,10.1,1.1c0.9,1,1.7,2.4,3.1,2.7"/>
+<path id="XMLID_5_" fill="none" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+	M502,350.7c3.8-1.4,6.1-5.1,8-8.6c1-1.8,2-3.7,3.6-5.1"/>
+<path id="XMLID_6_" fill="none" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+	M467,344.2c-4.7-2.5-10-3.6-15.3-3.3c-0.3,0.8-0.1,1.7,0.4,2.4c0.5,0.7,1.2,1.2,1.9,1.7c3.6,2.4,7.7,4.3,12,4.4
+	c1.3,0,2.7-0.1,3.8-0.9s1.8-2.2,1.4-3.4c-0.4-1.3-1.8-1.9-3.1-2.4c-3.3-1.3-6.6-2.6-10.1-2.7c-1.7-0.1-3.5,0.2-4.9,1.2"/>
+<path id="XMLID_7_" fill="none" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+	M504.5,339c-2.3-0.7-4.7-1.2-7.1-1.4c-1.9-0.2-3.9-0.3-5.5,0.5c-1.7,0.8-2.9,2.5-3,4.4c-0.1,1.9,1,3.7,2.8,4.4c1,0.4,2,0.4,3.1,0.4
+	c1.9,0,3.7,0,5.5-0.6c2.3-0.8,4.1-2.6,5.8-4.3c0.6-0.6,1.3-1.4,1.4-2.3c0-1.2-1.3-2.2-2.5-2.3c-1.2-0.1-2.4,0.5-3.6,1.1"/>
+<path id="XMLID_8_" fill="none" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+	M442.1,336.8c8.5,0.5,15.3-7.3,23.6-8.8c4.8-0.9,9.8,0.3,14.6-0.8c1.7-0.4,3.4-1.1,5.1-1.5c8.3-1.8,16.2,4,24.5,5.3"/>
+<path id="XMLID_9_" fill="none" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+	M542.5,344.8c-3.8-6.3-6.1-13.6-6.4-21"/>
+<path id="XMLID_10_" fill="none" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+	M502,290.6c9.3-11.1,24.3-15.6,35.4-24.8c3.6-2.9,6.7-6.4,9.8-9.8c9.3-10.1,18.7-20.4,29.8-28.4c6.1-4.4,14-8.2,20.9-5.1
+	c3.8,1.7,6.4,5.1,9.5,7.8c4.6,3.9,10.4,6.3,15,10.2c5.1,4.3,8.4,10.3,12.4,15.5c4.1,5.2,9.6,10.1,16.2,10.5"/>
+<path id="XMLID_11_" fill="none" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+	M569.1,250.8c3.2,6.2,10.2,9.4,17,11.3c6.8,1.9,13.9,2.8,19.9,6.4"/>
+<path id="XMLID_12_" fill="none" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+	M600.7,262.1c8.7,7.7,17.5,15.5,24,25.2c4,5.9,7.1,12.6,12.5,17.3c3.3,2.9,7.3,5,11,7.4c3.7,2.5,7.2,5.6,8.8,9.7
+	c0.7,1.7,1,3.6,1.9,5.3s2.7,3,4.6,2.5c1.6-3.1,1.3-7.2-0.7-10"/>
+<path id="XMLID_13_" fill="none" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+	M651.2,266c6.1,0.5,12.1,2.4,17.4,5.4c2.9,1.6,5.5,3.6,8.1,5.5c7.3,5.4,14.6,10.7,20.9,17.2c1.1,1.2,2.3,2.6,2,4.2
+	c-0.3,1.7-2.2,2.7-4,2.6c-1.7-0.1-3.3-1.1-4.7-2c-6-4-11.8-8.3-18.3-11.5"/>
+<path id="XMLID_14_" fill="none" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+	M655.3,314.5c10.6,4.6,21.2,9.1,31.9,13.5c1.5,0.6,3.2,1.3,4.8,1.1c1.7-0.2,3.3-1.5,3.2-3.2c-0.1-1.6-1.5-2.7-2.9-3.6
+	c-8.6-5.5-17.3-10.7-26.3-15.4"/>
+<path id="XMLID_15_" fill="none" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+	M668.8,295.2c9.6,9.1,23.7,12.5,32.9,22.1c1.5,1.6,3,3.8,2.5,5.9c-0.3,1.3-1.5,2.4-2.8,2.7c-1.3,0.3-2.8-0.3-3.7-1.3"/>
+<path id="XMLID_16_" fill="none" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+	M673.9,288c10.5,5.7,20.2,12.7,29,20.8c1.1,1,2.2,2.1,2.5,3.6s-1,3.2-2.5,2.9"/>
+<path id="XMLID_17_" fill="none" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+	M595.1,265.5c-2.8,2.9-4.6,6.5-7,9.7c-2.3,3.1-5.2,5.6-7.8,8.5c-6,6.5-10.4,14.5-16.7,20.7c-4.6,4.6-10.2,8.2-15.1,12.5
+	c-4.9,4.4-9.2,9.8-10.1,16.3"/>
+</svg>
+
+export default swimmer;
