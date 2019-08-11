f7ff4b2be268e4980b0b6de260d2c76c1d265388
reuvenaor <reuvenaor85@gmail.com>
2019-08-11 18:24:06 +0300

    Update waterHoraizon.js

--- components/waterHoraizon.js
+++ components/waterHoraizon.js
@@ -51,7 +51,7 @@ const firstPerson = (props) => {
             let quaternion = new THREE.Quaternion();
             quaternion.setFromEuler(eu);
 
-            let v = new THREE.Vector3(1, radius, -radius);
+            let v = new THREE.Vector3(1, 512, 512);
             // watching ground: 
             // let v = new THREE.Vector3(1, 1, 1); 
             v.applyQuaternion(quaternion);
@@ -75,7 +75,7 @@ const firstPerson = (props) => {
         scene = new THREE.Scene();
         //
         camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 1, 20000);
-        camera.position.set(1, radius, 1);
+        camera.position.set(1, 512, 1);
         //
         light = new THREE.DirectionalLight(0xffffff, 0.8);
         scene.add(light);
