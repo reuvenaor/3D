918217b0e106140024b0e43901808b39ca40ca6f
reuvenaor <reuvenaor85@gmail.com>
2019-08-11 18:21:46 +0300

    Update waterHoraizon.js

--- components/waterHoraizon.js
+++ components/waterHoraizon.js
@@ -51,7 +51,7 @@ const firstPerson = (props) => {
             let quaternion = new THREE.Quaternion();
             quaternion.setFromEuler(eu);
 
-            let v = new THREE.Vector3(1, radius, radius);
+            let v = new THREE.Vector3(1, radius, -radius);
             // watching ground: 
             // let v = new THREE.Vector3(1, 1, 1); 
             v.applyQuaternion(quaternion);
@@ -59,7 +59,7 @@ const firstPerson = (props) => {
             console.log('contoler', controls);
             if (alpha && beta && gamma && controls) {
                 controls.lookAt(v.y, v.x, v.z);
-                 controls.update();
+                controls.update();
             }
         }
     }
